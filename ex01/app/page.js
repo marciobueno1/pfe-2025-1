9644fbe69972ebef9630f4ff43c16c9674beba80
Marcio Bueno <marciobueno@gmail.com>
2025-02-25 00:25:37 +0000

    Adicionado botão com número aleatório

--- ex01/app/page.js
+++ ex01/app/page.js
@@ -6,13 +6,22 @@ function Texto1() {
   return <p>Um outro parágrafo de texto!</p>;
 }
 
+function gerarNumAleat() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
 export default function Home() {
   const [hide, setHide] = useState(false);
+  const [numAleat, setNumAleat] = useState(gerarNumAleat());
   const aula = 2;
   return (
     <div>
       <h1>Olá Turma!!!!</h1>
       <p>Essa é a nossa {aula}ª aula de React!</p>
+      <hr />
+      <button onClick={() => setNumAleat(gerarNumAleat())}>
+        Aletatório: {numAleat}
+      </button>
       <hr />
       <button onClick={() => setHide(!hide)}>{hide ? "Show" : "Hide"}</button>
       <hr />
